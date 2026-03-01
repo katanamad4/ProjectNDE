eebe0624f42e60ced1d5c85ff8f8637a0a292413
katanamad <katanamad4@gmail.com>
2026-03-01 17:53:55 +0200

    made a better debug hud system

--- debug_hud.lua
+++ debug_hud.lua
@@ -3,26 +3,41 @@ local entities = require("entities")
 
 local debug = {}
 
+debug.line_spacing = 15
+
+debug.entries = {
+    fps = function(self, y)
+            love.graphics.print("FPS: " .. love.timer.getFPS(), 10, y)
+    end,
+    entities = function(self, y)
+        love.graphics.print("Entities: " .. #entities, 10, y)
+    end,
+    movement_v = function(self, y)
+        if state.movement_vector then
+            love.graphics.print(
+                "Movement: " ..
+                math.floor(state.movement_vector.x) .. ", " ..
+                math.floor(state.movement_vector.y),
+                10, y
+            )
+        end
+    end,
+    lives = function(self, y)
+        love.graphics.print("Lives:" .. state.lives .. " invincible:" .. entities.player.invincible, 10, y)
+    end,
+}
+
+
 
 function debug.draw()
     love.graphics.setColor(state.palette.green)
-
-    love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 10)
-    love.graphics.print("Entities: " .. #entities, 10, 25)
-
-    if state.movement_vector then
-        love.graphics.print(
-            "Movement: " ..
-            math.floor(state.movement_vector.x) .. ", " ..
-            math.floor(state.movement_vector.y),
-            10, 40
-        )
+    local n = 0
+    for _, entry in pairs(debug.entries) do
+        n = n + 1
+        entry(debug, n * debug.line_spacing + 5)
     end
-    love.graphics.print("Lives:" .. state.lives .. "invincible:" .. entities.player.invincible)
 
-    -- for _, entity in ipairs(entities) do
-    --     love.graphics.print()
-    -- end 
+    
 
 end
 
--- entities.lua
+++ entities.lua
@@ -9,7 +9,7 @@ local player_entity = player(vector.new(300, 300), "goob")
 entities.player = player_entity      -- named reference
 table.insert(entities, player_entity) -- numeric list entry
 
-table.insert(entities, bullet(vector.new(100, 100), vector.new(2, 1), vector.new(), 3))
+table.insert(entities, bullet(vector.new(100, 100), vector.new(0, 0), vector.new(), 3))
 table.insert(entities, bullet(vector.new(200, 100), vector.new(2, 1), vector.new(), 3))
 
 
